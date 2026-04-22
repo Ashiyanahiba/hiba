888496e881fd9a3a764c1c6b8577c9a5ffc067a3
Ashiyanahiba <ashiyanahiba1@gmail.com>
2026-04-22 10:55:48 +0530

    Update app.py

--- selfhealinghealthcare/app.py
+++ selfhealinghealthcare/app.py
@@ -12,21 +12,17 @@ st.set_page_config(page_title="Healthcare Dashboard", layout="wide")
 st.markdown("""
 <style>
 
-/* REMOVE TOP SPACE */
 .block-container {
     padding-top: 1rem !important;
 }
 
-/* REMOVE HEADER SPACE */
 header {visibility: hidden;}
 
-/* MAIN BACKGROUND */
 .stApp {
     background: linear-gradient(135deg, #ff9a9e, #fad0c4);
     color: black;
 }
 
-/* SIDEBAR STYLE */
 [data-testid="stSidebar"] {
     background: linear-gradient(180deg, #ff758c, #ff7eb3);
     color: white;
@@ -37,14 +33,12 @@ header {visibility: hidden;}
     color: white !important;
 }
 
-/* TEXT AREA */
 textarea {
     color: black !important;
     background-color: #ffffff !important;
     border-radius: 10px;
 }
 
-/* KPI CARDS */
 div[data-testid="stMetric"] {
     background-color: rgba(255,255,255,0.6);
     padding: 15px;
@@ -67,7 +61,6 @@ if "logs" not in st.session_state:
 if "cpu_data" not in st.session_state:
     st.session_state.cpu_data = []
 
-# BUTTONS
 if st.sidebar.button("▶ Start Monitoring"):
     st.session_state.running = True
     st.toast("Monitoring Started 💗")
